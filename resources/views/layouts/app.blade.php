711f5dfa5990de0aaaca8fc2e08d0553c046892e
Joe Doherty <papaverjosepherum@gmail.com>
2025-05-21 02:19:09 +0000

    Return to listing with title link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <div id="app">
             <header class="bg-gray-800 pt-8 pb-20">
                 <div class="w-9/12 text-center mr-auto ml-auto -mt-0 mb-0">
-                    <h1 class="text-orange text-5xl p-10">Book Shop</h1>
+                    <a href="/" class="text-orange text-5xl p-10 inline-block">Book Shop</a>
                     <p class="w-9/12 mr-auto ml-auto -mt-0 mb-0 text-white">Cupcake ipsum dolor sit amet croissant. I love topping candy canes sweet roll croissant caramels. Soufflé macaroon liquorice chocolate tart I love.</p>
                 </div>
             </header>
