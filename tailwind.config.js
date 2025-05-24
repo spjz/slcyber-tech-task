7e06aec0ef3ab28753c10f8e4fbeba47c1b2a40a
Joe Doherty <papaverjosepherum@gmail.com>
2025-05-24 19:55:09 +0000

    Add blade views to tailwind config to fix header style not being applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-    './resources/js/**/*.{js,vue}',
-];
-export const theme = {
-    extend: {
-        colors: {
-            'orange': '#E94520',
-            'blue': '#1f2937',
-            'grey': '#d1d5db',
-            'white': '#ffffff',
-            'black': '#000000',
-        }
+module.exports = {
+    content: [
+        './resources/js/**/*.{js,vue}',
+        './resources/views/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'orange': '#E94520',
+                'blue': '#1f2937',
+                'grey': '#d1d5db',
+                'white': '#ffffff',
+                'black': '#000000',
+            }
+        },
     },
+    plugins: [],
 };
-export const plugins = [];
 
